d0114951518dd6949957c8ebfc812534ca6a2d36
Somnath477 <80127035+Somnath477@users.noreply.github.com>
2025-12-02 13:51:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
-    await connectDB();
-    console.log("Server running on port", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
